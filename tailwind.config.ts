8236ed7b99e43949356d82b468daf4d7e41cd6a8
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2023-12-27 11:48:02 +0100

    Adding shadcn to the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,15 @@ import { type Config } from 'tailwindcss'
 import typographyStyles from './typography'
 import typographyPlugin from '@tailwindcss/typography'
 import headlessuiPlugin from '@headlessui/tailwindcss'
+import tailwindcssAnimatePlugin from 'tailwindcss-animate'
 
 export default {
-  content: ['./src/**/*.{js,mjs,jsx,ts,tsx,mdx}'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{js,mjs,jsx,ts,tsx,mdx}'
+  ],
   darkMode: 'class',
   theme: {
     fontSize: {
@@ -23,6 +29,13 @@ export default {
       '8xl': ['6rem', { lineHeight: '1' }],
       '9xl': ['8rem', { lineHeight: '1' }],
     },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      }
+    },
     typography: typographyStyles,
     extend: {
       boxShadow: {
@@ -40,7 +53,25 @@ export default {
         7.5: '0.075',
         15: '0.15',
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [typographyPlugin, headlessuiPlugin],
+  plugins: [
+    typographyPlugin, 
+    headlessuiPlugin, 
+    // tailwindcssAnimatePlugin
+  ],
 } satisfies Config
