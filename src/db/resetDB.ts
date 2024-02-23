cb8d59cfcc64396318bf876cca47184ab0aa7a85
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2024-02-23 10:55:45 +0100

    Create a resetDB functionality

---
+++ src/db/resetDB.ts
@@ -0,0 +1,131 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import { assignments, companies, users, problems, programming_languages } from "@/db/schema";
+import { supportedLangs } from "@/app/supportedIDEConfigs";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+async function resetDB(): Promise<void> {
+  const db = drizzle(sql);
+
+  // Deletion happens in cascade
+  await db.delete(companies);
+
+  const insertedCompany = await db
+    .insert(companies)
+    .values({
+      name: "Emma Sleep",
+    })
+    .returning({ id: companies.id, name: companies.name });
+
+  const user = await db
+    .insert(users)
+    .values({
+      authId: process.env.CLERK_SAMPLE_USER_ID,
+      companyId: insertedCompany[0].id,
+    })
+    .returning({
+      id: users.id,
+      authId: users.authId,
+      companyId: users.companyId,
+    });
+
+  await db.insert(programming_languages).values(supportedLangs.map((lang) => ({ id: lang.id, name: lang.language })));
+
+  const problem = await db
+  .insert(problems)
+  .values({
+    title: "Minimum Time to Make Rope Colorful",
+    description: `
+### [1578. Minimum Time to Make Rope Colorful](https://leetcode.com/problems/minimum-time-to-make-rope-colorful/?envType=daily-question&envId=2023-12-27)
+
+Difficulty: Medium
+
+Likes: 3.7K | Dislikes: 129
+
+---
+
+Alice has \`n\` balloons arranged on a rope. You are given a **0-indexed** string \`colors\` where \`colors[i]\` is the color of the \`i^th\` balloon.
+
+Alice wants the rope to be **colorful**. She does not want **two consecutive balloons** to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it **colorful**. You are given a **0-indexed** integer array \`neededTime\` where \`neededTime[i]\` is the time (in seconds) that Bob needs to remove the \`i^th\` balloon from the rope.
+
+_Return the **minimum time** Bob needs to make the rope **colorful**._
+
+### Example 1:
+
+![Balloon Example 1](https://assets.leetcode.com/uploads/2021/12/13/ballon1.jpg)
+
+\`\`\`plaintext
+Input: colors = "abaac", neededTime = [1,2,3,4,5]
+Output: 3
+Explanation: In the above image, 'a' is blue, 'b' is red, and 'c' is green.
+Bob can remove the blue balloon at index 2. This takes 3 seconds.
+There are no longer two consecutive balloons of the same color. Total time = 3.
+\`\`\`
+### Example 2:
+
+![Balloon Example 2](https://assets.leetcode.com/uploads/2021/12/13/balloon2.jpg)
+
+\`\`\`plaintext
+Input: colors = "abc", neededTime = [1,2,3]
+Output: 0
+Explanation: The rope is already colorful. Bob does not need to remove any balloons from the rope.
+\`\`\`
+### Example 3:
+
+![Balloon Example 3](https://assets.leetcode.com/uploads/2021/12/13/balloon3.jpg)
+
+\`\`\`plaintext
+Input: colors = "aabaa", neededTime = [1,2,3,4,1]
+Output: 2
+Explanation: Bob will remove the balloons at indices 0 and 4. Each balloon takes 1 second to remove.
+There are no longer two consecutive balloons of the same color. Total time = 1 + 1 = 2.
+\`\`\`
+### Constraints:
+
+- \`n == colors.length == neededTime.length\`
+- \`1 <= n <= 10^5\`
+- \`1 <= neededTime[i] <= 10^4\`
+- \`colors\` contains only lowercase English letters.
+
+---
+
+Accepted: 243.5K | Submissions: 380.4K | Acceptance Rate: 64.0%
+
+---
+
+Related Topics: [Array](/tag/array/), [String](/tag/string/), [Dynamic Programming](/tag/dynamic-programming/), [Greedy](/tag/greedy/)
+
+---
+
+Copyright ©️ 2023 LeetCode All rights reserved
+    `,
+  }).returning({
+    id: problems.id,
+    title: problems.title,
+    description: problems.description,
+  });
+
+
+
+  const assignment = await db
+    .insert(assignments)
+    .values({
+      title: "Assignment 1",
+      hash: "Emma Sleep",
+      companyId: user[0].companyId,
+      problemId: problem[0].id,
+    })
+    .returning({
+      id: assignments.id,
+      title: assignments.title,
+      hash: assignments.hash,
+      companyId: assignments.companyId,
+      problemId: assignments.problemId,
+    });
+}
+
+resetDB().then(() => {
+  console.log("Database reset");
+});
