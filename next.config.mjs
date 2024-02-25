ca967ca713a743e1bbf7bb59c4b0ee86fe676067
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2024-02-25 14:22:57 +0100

    Configure CORS in nextjs.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
-import nextMDX from '@next/mdx'
+import nextMDX from "@next/mdx";
 
-import { recmaPlugins } from './src/mdx/recma.mjs'
-import { rehypePlugins } from './src/mdx/rehype.mjs'
-import { remarkPlugins } from './src/mdx/remark.mjs'
-import withSearch from './src/mdx/search.mjs'
+import { recmaPlugins } from "./src/mdx/recma.mjs";
+import { rehypePlugins } from "./src/mdx/rehype.mjs";
+import { remarkPlugins } from "./src/mdx/remark.mjs";
+import withSearch from "./src/mdx/search.mjs";
 
 const withMDX = nextMDX({
   options: {
@@ -11,22 +11,42 @@ const withMDX = nextMDX({
     rehypePlugins,
     recmaPlugins,
   },
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
   images: {
     remotePatterns: [
       { hostname: "avatars.githubusercontent.com", pathname: "/u/*" },
       { hostname: "cdn.discordapp.com", pathname: "/avatars/*" },
       { hostname: "*.googleusercontent.com", pathname: "/a/*" },
-      { hostname: '*.gravatar.com', pathname: '/**' },
+      { hostname: "*.gravatar.com", pathname: "/**" },
       { hostname: "media.licdn.com", pathname: "**" },
       { hostname: "uploadthing.com", pathname: "/f/**" },
       { hostname: "utfs.io", pathname: "/f/**" },
     ],
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default withSearch(withMDX(nextConfig))
+export default withSearch(withMDX(nextConfig));
