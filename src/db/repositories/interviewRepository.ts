928a955e5651ee9d5a535ab9ac4871b89e56301c
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2024-03-23 18:18:58 +0100

    WIP

--- src/db/repositories/interviewRepository.ts
+++ src/db/repositories/interviewRepository.ts
@@ -1,6 +1,6 @@
 import { VercelPgDatabase } from "drizzle-orm/vercel-postgres";
-import { interviewsTable } from "@/db/schema";
-import { eq, and } from "drizzle-orm";
+import { interviewsTable, organizationsTable, problemsTable, submissionsTable, transcriptionsTable, usersTable } from "@/db/schema";
+import { eq, and, count } from "drizzle-orm";
 import * as schema from "@/db/schema";
 
 export type Interview = {
@@ -69,4 +69,44 @@ export async function insertInterview(
       createdAt: interviewsTable.createdAt,
       updatedAt: interviewsTable.updatedAt,
     });
+}
+
+export async function getAllInterviews(
+  db: VercelPgDatabase<Record<string, never>>,
+  userId: string
+) {
+  return await db
+    .select()
+    .from(interviewsTable)
+    .leftJoin(
+      organizationsTable,
+      eq(interviewsTable.organizationId, organizationsTable.id)
+    )
+    .leftJoin(usersTable, eq(usersTable.organizationId, organizationsTable.id))
+    .leftJoin(problemsTable, eq(problemsTable.id, interviewsTable.problemId))
+    .leftJoin(submissionsTable, eq(submissionsTable.interviewId, interviewsTable.id))
+    .where(eq(usersTable.externalId, userId));
+}
+
+export async function getUpcomingInterviews(
+  db: VercelPgDatabase<Record<string, never>>,
+  userId: string
+) {
+  return await db
+    .select()
+    .from(interviewsTable)
+    .leftJoin(
+      organizationsTable,
+      eq(interviewsTable.organizationId, organizationsTable.id)
+    )
+    .leftJoin(usersTable, eq(usersTable.organizationId, organizationsTable.id))
+    .leftJoin(transcriptionsTable, eq(transcriptionsTable.interviewId, interviewsTable.id))
+    .leftJoin(problemsTable, eq(problemsTable.id, interviewsTable.problemId))
+    .leftJoin(submissionsTable, eq(submissionsTable.interviewId, interviewsTable.id))
+    .where(
+      and(
+        eq(usersTable.externalId, userId),
+        eq(count(transcriptionsTable.interviewId), 0)
+      )
+    );
 }
