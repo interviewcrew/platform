c3436cf37672bafc5c0ac104192be57cf44a0869
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2024-04-22 19:29:29 +0200

    Update resetDB to create initial DB

--- src/db/resetDB.ts
+++ src/db/resetDB.ts
@@ -1,72 +1,31 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import {
-  interviewsTable,
   organizationsTable,
   usersTable,
   problemsTable,
   programmingLanguagesTable,
+  languagesTable,
+  evaluationMetricsTable,
 } from "@/db/schema";
 import { supportedLangs } from "@/app/supportedIDEConfigs";
 import dotenv from "dotenv";
+import * as schema from "@/db/schema";
 
 dotenv.config({ path: ".env.local" });
 
 async function resetDB(): Promise<void> {
-  const db = drizzle(sql);
-
-  // Deletion happens in cascade
-  await db.delete(organizationsTable);
-  await db.delete(programmingLanguagesTable);
+  const db = drizzle(sql, { schema });
 
   if (process.env.NODE_ENV === "production") {
     throw new Error("Resetting the database is not allowed in production");
   }
-
-  if (
-    !process.env.CLERK_SAMPLE_ORG_ID1 ||
-    !process.env.CLERK_SAMPLE_ORG_ID2 ||
-    !process.env.CLERK_SAMPLE_USER_ID1 ||
-    !process.env.CLERK_SAMPLE_USER_ID2
-  ) {
-    throw new Error(
-      "Please provide the organization and user id in the .env file"
-    );
-  }
-
-  const insertedOrganizations = await db
-    .insert(organizationsTable)
-    .values([
-      {
-        name: "InterviewCrew 1",
-        externaleId: process.env.CLERK_SAMPLE_ORG_ID1,
-        slug: "interviewcrew-1",
-      },
-      {
-        name: "InterviewCrew 2",
-        externaleId: process.env.CLERK_SAMPLE_ORG_ID2,
-        slug: "interviewcrew-2",
-      },
-    ])
-    .returning({ id: organizationsTable.id, name: organizationsTable.name });
-
-  const user = await db
-    .insert(usersTable)
-    .values([
-      {
-        externalId: process.env.CLERK_SAMPLE_USER_ID1,
-        organizationId: insertedOrganizations[0].id,
-      },
-      {
-        externalId: process.env.CLERK_SAMPLE_USER_ID2,
-        organizationId: insertedOrganizations[1].id,
-      },
-    ])
-    .returning({
-      id: usersTable.id,
-      externalId: usersTable.externalId,
-      organizationId: usersTable.organizationId,
-    });
+  
+  // Deletion happens in cascade
+  await db.delete(organizationsTable);
+  await db.delete(programmingLanguagesTable);
+  await db.delete(languagesTable);
+  await db.delete(evaluationMetricsTable);
 
   await db
     .insert(programmingLanguagesTable)
@@ -74,96 +33,13 @@ async function resetDB(): Promise<void> {
       supportedLangs.map((lang) => ({ id: lang.id, name: lang.language }))
     );
 
-  const problem = await db
-    .insert(problemsTable)
+  await db
+    .insert(languagesTable)
     .values({
-      title: "Minimum Time to Make Rope Colorful",
-      description: `
-### [1578. Minimum Time to Make Rope Colorful](https://leetcode.com/problems/minimum-time-to-make-rope-colorful/?envType=daily-question&envId=2023-12-27)
-
-Difficulty: Medium
-
-Likes: 3.7K | Dislikes: 129
-
----
-
-Alice has \`n\` balloons arranged on a rope. You are given a **0-indexed** string \`colors\` where \`colors[i]\` is the color of the \`i^th\` balloon.
-
-Alice wants the rope to be **colorful**. She does not want **two consecutive balloons** to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it **colorful**. You are given a **0-indexed** integer array \`neededTime\` where \`neededTime[i]\` is the time (in seconds) that Bob needs to remove the \`i^th\` balloon from the rope.
-
-_Return the **minimum time** Bob needs to make the rope **colorful**._
-
-### Example 1:
-
-![Balloon Example 1](https://assets.leetcode.com/uploads/2021/12/13/ballon1.jpg)
-
-\`\`\`plaintext
-Input: colors = "abaac", neededTime = [1,2,3,4,5]
-Output: 3
-Explanation: In the above image, 'a' is blue, 'b' is red, and 'c' is green.
-Bob can remove the blue balloon at index 2. This takes 3 seconds.
-There are no longer two consecutive balloons of the same color. Total time = 3.
-\`\`\`
-### Example 2:
-
-![Balloon Example 2](https://assets.leetcode.com/uploads/2021/12/13/balloon2.jpg)
-
-\`\`\`plaintext
-Input: colors = "abc", neededTime = [1,2,3]
-Output: 0
-Explanation: The rope is already colorful. Bob does not need to remove any balloons from the rope.
-\`\`\`
-### Example 3:
-
-![Balloon Example 3](https://assets.leetcode.com/uploads/2021/12/13/balloon3.jpg)
-
-\`\`\`plaintext
-Input: colors = "aabaa", neededTime = [1,2,3,4,1]
-Output: 2
-Explanation: Bob will remove the balloons at indices 0 and 4. Each balloon takes 1 second to remove.
-There are no longer two consecutive balloons of the same color. Total time = 1 + 1 = 2.
-\`\`\`
-### Constraints:
-
-- \`n == colors.length == neededTime.length\`
-- \`1 <= n <= 10^5\`
-- \`1 <= neededTime[i] <= 10^4\`
-- \`colors\` contains only lowercase English letters.
-
----
-
-Accepted: 243.5K | Submissions: 380.4K | Acceptance Rate: 64.0%
-
----
-
-Related Topics: [Array](/tag/array/), [String](/tag/string/), [Dynamic Programming](/tag/dynamic-programming/), [Greedy](/tag/greedy/)
-
----
-
-Copyright ©️ 2023 LeetCode All rights reserved
-    `,
+      name: "English",
     })
-    .returning({
-      id: problemsTable.id,
-      title: problemsTable.title,
-      description: problemsTable.description,
-    });
+    .returning({ id: languagesTable.id, name: languagesTable.name });
 
-  const interview = await db
-    .insert(interviewsTable)
-    .values({
-      title: "Interview 1",
-      hash: "dpv-yhep-xer",
-      organizationId: user[0].organizationId,
-      problemId: problem[0].id,
-    })
-    .returning({
-      id: interviewsTable.id,
-      title: interviewsTable.title,
-      hash: interviewsTable.hash,
-      organizationId: interviewsTable.organizationId,
-      problemId: interviewsTable.problemId,
-    });
 }
 
 resetDB().then(() => {
