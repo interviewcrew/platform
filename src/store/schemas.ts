8da4a0759017d1cf96eec424bab455481ce98722
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2024-04-23 22:42:25 +0200

    Redirect from landing to dashboard if login

--- src/store/schemas.ts
+++ src/store/schemas.ts
@@ -1,5 +1,20 @@
+import { User as ClerkUser } from "@clerk/nextjs/server";
+
 export interface User {
   fullName: string;
   imageUrl: string;
   primaryEmailAddress: string;
+}
+
+export function convertClerkUserToUser(
+  loadedUser: ClerkUser
+): User {
+  return {
+    fullName: `${loadedUser.firstName} ${loadedUser.lastName}`,
+    imageUrl: loadedUser.imageUrl,
+    primaryEmailAddress:
+      loadedUser.emailAddresses.find(
+        (email: any) => email.id == loadedUser.primaryEmailAddressId
+      )?.emailAddress ?? "",
+  };
 }
