8ae6b1c1916cad53e791d99da3e5a87516d0df9b
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2024-05-02 21:27:07 +0200

    Update the pricing to a package and format the file

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,10 +1,10 @@
-import clsx from 'clsx'
+import clsx from "clsx";
 
-import { Button } from '@/components/Button'
-import { Container } from '@/components/Container'
-import { CheckCircle2 as CheckCircle , XCircle } from 'lucide-react'
+import { Button } from "@/components/Button";
+import { Container } from "@/components/Container";
+import { CheckCircle2 as CheckCircle, XCircle } from "lucide-react";
 
-function SwirlyDoodle(props: React.ComponentPropsWithoutRef<'svg'>) {
+function SwirlyDoodle(props: React.ComponentPropsWithoutRef<"svg">) {
   return (
     <svg
       aria-hidden="true"
@@ -18,7 +18,7 @@ function SwirlyDoodle(props: React.ComponentPropsWithoutRef<'svg'>) {
         d="M240.172 22.994c-8.007 1.246-15.477 2.23-31.26 4.114-18.506 2.21-26.323 2.977-34.487 3.386-2.971.149-3.727.324-6.566 1.523-15.124 6.388-43.775 9.404-69.425 7.31-26.207-2.14-50.986-7.103-78-15.624C10.912 20.7.988 16.143.734 14.657c-.066-.381.043-.344 1.324.456 10.423 6.506 49.649 16.322 77.8 19.468 23.708 2.65 38.249 2.95 55.821 1.156 9.407-.962 24.451-3.773 25.101-4.692.074-.104.053-.155-.058-.135-1.062.195-13.863-.271-18.848-.687-16.681-1.389-28.722-4.345-38.142-9.364-15.294-8.15-7.298-19.232 14.802-20.514 16.095-.934 32.793 1.517 47.423 6.96 13.524 5.033 17.942 12.326 11.463 18.922l-.859.874.697-.006c2.681-.026 15.304-1.302 29.208-2.953 25.845-3.07 35.659-4.519 54.027-7.978 9.863-1.858 11.021-2.048 13.055-2.145a61.901 61.901 0 0 0 4.506-.417c1.891-.259 2.151-.267 1.543-.047-.402.145-2.33.913-4.285 1.707-4.635 1.882-5.202 2.07-8.736 2.903-3.414.805-19.773 3.797-26.404 4.829Zm40.321-9.93c.1-.066.231-.085.29-.041.059.043-.024.096-.183.119-.177.024-.219-.007-.107-.079ZM172.299 26.22c9.364-6.058 5.161-12.039-12.304-17.51-11.656-3.653-23.145-5.47-35.243-5.576-22.552-.198-33.577 7.462-21.321 14.814 12.012 7.205 32.994 10.557 61.531 9.831 4.563-.116 5.372-.288 7.337-1.559Z"
       />
     </svg>
-  )
+  );
 }
 
 function Plan({
@@ -31,51 +31,58 @@ function Plan({
   features,
   featured = false,
 }: {
-  name: string
-  price: string
-  description: string
-  callToAction: string
-  showPerInterview: boolean
-  href: string
-  features: Array<string>
-  featured?: boolean
+  name: string;
+  price: string;
+  description: string;
+  callToAction: string;
+  showPerInterview: boolean;
+  href: string;
+  features: Array<string>;
+  featured?: boolean;
 }) {
   return (
     <section
       className={clsx(
-        'flex flex-col rounded-3xl px-6 sm:px-8',
-        featured ? 'order-first bg-blue-600 py-8 lg:order-none' : 'lg:py-8',
+        "flex flex-col rounded-3xl px-6 sm:px-8",
+        featured ? "order-first bg-blue-600 py-8 lg:order-none" : "lg:py-8"
       )}
     >
       <h3 className="mt-5 font-display text-lg text-white">{name}</h3>
       <p
         className={clsx(
-          'mt-2 text-base',
-          featured ? 'text-white' : 'text-slate-400',
+          "mt-2 text-base",
+          featured ? "text-white" : "text-slate-400"
         )}
       >
         {description}
       </p>
       <p className="order-first font-display text-5xl font-light tracking-tight text-white">
-        {price} {showPerInterview && <span className="text-base font-normal">/ per interview</span>}
+        {price}{" "}
+        {showPerInterview && (
+          <span className="text-base font-normal">
+            / per <span className="font-bold">10</span> interviews
+          </span>
+        )}
       </p>
       <ul
         role="list"
         className={clsx(
-          'order-last mt-10 flex flex-col gap-y-3 text-sm',
-          featured ? 'text-white' : 'text-slate-200',
+          "order-last mt-10 flex flex-col gap-y-3 text-sm",
+          featured ? "text-white" : "text-slate-200"
         )}
       >
         {features.map((feature) => (
           <li key={feature} className="flex">
-            <CheckCircle className={featured ? 'text-white' : 'text-slate-400'} />
+            <CheckCircle
+              className={featured ? "text-white" : "text-slate-400"}
+            />
             <span className="ml-4">{feature}</span>
           </li>
         ))}
       </ul>
       <Button
         href={href}
-        variant={featured ? 'solid' : 'outline'}
+        variant={featured ? "solid" : "outline"}
         color="white"
         className="mt-8"
         aria-label={`Get started with the ${name} plan for ${price}`}
@@ -83,7 +90,7 @@ function Plan({
         {callToAction}
       </Button>
     </section>
-  )
+  );
 }
 
 export function Pricing() {
@@ -99,72 +106,73 @@ export function Pricing() {
             <span className="relative whitespace-nowrap">
               <SwirlyDoodle className="absolute left-0 top-1/2 h-[1em] w-full fill-blue-400" />
               <span className="relative">Simple pricing,</span>
-            </span>{' '}
+            </span>{" "}
             for everyone.
           </h2>
           <p className="mt-4 text-lg text-slate-400">
-            Start using InterviewCrew today and pay-as-you-go. If it&apos;s not for you, no worries, we&apos;ll give you your money back.
+            Start using InterviewCrew today and pay-as-you-go. If it&apos;s not
+            for you, no worries, we&apos;ll give you your money back.
           </p>
         </div>
         <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
           <Plan
             name="Bring your own OpenAI"
-            price="€5"
+            price="€50"
             description="Use InterviewCrew with your own OpenAI credentials"
             callToAction="Get started, risk free"
             showPerInterview={true}
             href="/register"
             features={[
-              'Candidate evaluation and feedback',
-              'Questions based on your job description',
-              'Questions based on candidates resume',
-              'Generating follow up questions',
-              'Answer candidates questions',
-              'Online coding environment',
-              'Live coding assessment',
-              'Only Google Meet',
+              "Candidate evaluation and feedback",
+              "Questions based on your job description",
+              "Questions based on candidates resume",
+              "Generating follow up questions",
+              "Answer candidates questions",
+              "Online coding environment",
+              "Live coding assessment",
+              "Only Google Meet",
             ]}
           />
           <Plan
             featured
             name="The most convinient"
-            price="€10"
+            price="€100"
             description="Get started instantly with our fully managed solution"
             callToAction="Get started, risk free"
             showPerInterview={true}
             href="/register"
             features={[
-              'We handle the OpenAI API keys and tokens',
-              'Complete access to the problems library',
-              'Candidate evaluation and feedback',
-              'Questions based on your job description',
-              'Questions based on candidates resume',
-              'Generating follow up questions',
-              'Answer candidates questions',
-              'Online coding environment',
-              'Live coding assessment',
-              'Only Google Meet',
+              "We handle the OpenAI API keys and tokens",
+              "Complete access to the problems library",
+              "Candidate evaluation and feedback",
+              "Questions based on your job description",
+              "Questions based on candidates resume",
+              "Generating follow up questions",
+              "Answer candidates questions",
+              "Online coding environment",
+              "Live coding assessment",
+              "Only Google Meet",
             ]}
           />
           <Plan
             name="Enterprise"
             price="Contact us"
             description="Everything you need to nail interviews, at scale"
-            callToAction='Contact us'
+            callToAction="Contact us"
             showPerInterview={false}
             href="/contact-us"
             features={[
-              'Everything in the most convinient plan',
-              'Check candidate against all job listings',
-              'Integration with your ATSs',
-              'Your interview platform(Zoom, Teams, etc.)',
-              'Total flexibility on the model',
-              'White label IDE during the interview',
-              'SSO with your organization',
+              "Everything in the most convinient plan",
+              "Check candidate against all job listings",
+              "Integration with your ATSs",
+              "Your interview platform(Zoom, Teams, etc.)",
+              "Total flexibility on the model",
+              "White label IDE during the interview",
+              "SSO with your organization",
             ]}
           />
         </div>
       </Container>
     </section>
-  )
+  );
 }
