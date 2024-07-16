3b2a94136bc37b05a2b2f7b569216dd008aed8f3
Mehdi Kazemi <mehdi.kazemi8@gmail.com>
2024-07-16 11:34:13 -0700

    Fix Secondary Features messaging

--- src/components/SecondaryFeatures.tsx
+++ src/components/SecondaryFeatures.tsx
@@ -20,10 +20,10 @@ interface Feature {
 
 const features: Array<Feature> = [
   {
-    name: "Broaden your hiring funnel",
-    summary: "Evaluate the candidate against all job listings with one interview",
+    name: "Step One",
+    summary: "Understanding Your Company and Your Needs",
     description:
-      "Using InterviewCrew, you can interview the candidate once, and then evaluate the candidate against all the job listings in your organization.",
+      "We will have multiple sessions together to learn about your business completely. We will create the proper job ad based on your needs and train our engineers to represent you in the interview process.",
     image: screenshotProfitLoss,
     icon: function ReportingIcon() {
       return (
@@ -47,11 +47,10 @@ const features: Array<Feature> = [
     },
   },
   {
-    name: "White label IDE during the interview",
-    summary:
-      "Do the interview in our platform, with your own branding",
+    name: "Step Two",
+    summary: "Finding top technical talents for your company",
     description:
-      "Using InterviewCrew, you can do the interview in our platform, with your own branded IDE. The candidate would feel like they are doing the interview in your platform.",
+      "We publish the job ad we created based on your needs, review all the resumes, schedule multiple rounds of technical interviews, handle all communication with the candidates, and ensure we find the top technical talent for your company.",
     image: screenshotInventory,
     icon: function InventoryIcon() {
       let id = useId();
@@ -83,10 +82,11 @@ const features: Array<Feature> = [
     },
   },
   {
-    name: "ATS integrations and SSO support",
-    summary: "Sync the interview results back to your favorite ATS",
+    name: "Step Three",
+    summary:
+      "You evaluate the candidates who passed the technical interviews for cultural fit",
     description:
-      "Using InterviewCrew, updating the status of a candidate in the ATS after an interview would be as easy as pressing a button",
+      "Since cultural fit is crucial for startups, we want you to conduct this round to ensure the hire aligns with your culture, in addition to being technically proficient. You will then make the final hiring decision.",
     image: screenshotContacts,
     icon: function ContactsIcon() {
       return (
@@ -197,7 +197,8 @@ export function SecondaryFeatures() {
       <Container>
         <div className="mx-auto max-w-2xl md:text-center">
           <h2 className="font-display tracking-tight text-slate-900 text-4xl">
-            Everything your organization needs for a
+            We're not just another agency; we are an extension of your company,
+            offering a{" "}
             <span className="relative whitespace-nowrap text-blue-600">
               <svg
                 aria-hidden="true"
@@ -207,14 +208,10 @@ export function SecondaryFeatures() {
               >
                 <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
               </svg>
-              <span className="relative">successful hire</span>
+              <span className="relative">tailored interview</span>
             </span>{" "}
-            in our enterprise plan
+            pipeline designed specifically based on your needs
           </h2>
-          <p className="mt-4 text-lg tracking-tight text-slate-700">
-            InterviewCrew would be equiped with all the enterprise features that
-            your organization needs to make the best hiring decisions.
-          </p>
         </div>
         <FeaturesMobile />
         <FeaturesDesktop />
